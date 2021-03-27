b782b96f47f97bf297ba1502e71b8a4faa48fec3
Vlad <vladikdavidov98>
2021-03-27 17:39:15 +0200

    compose Array

--- js/index.js
+++ js/index.js
@@ -1,40 +1,64 @@
+
 let btn = document.getElementById('btn-upload-csv').addEventListener('click',()=>{
   Papa.parse(document.getElementById('upload-csv').files[0],{
     download:true,
     header:false,
     complete: function(results){
-      console.log(results);
-      let i = 0;
-      results.data.map((data,index)=>{
-        if(i === 0){
-          let table = document.getElementById("tbl-data");
-          generateTableHead(table,data);
-        }else{
-          let table = document.getElementById("tbl-data");
-          generateTableHead(table,data);
-        }
-        i++
-      });
+      main(results.data);
+
     }
   });
 });
 
-function generateTableHead(table,data){
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for(let key of data){
-    let th=document.createElement('th');
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
+function main(initialData){
+  removeHeader(initialData);
+  let finaliData = composeFinaliData(initialData);
+  console.log(finaliData);
 }
 
-function generateTableRows(table,data){
-  let newRow = table.insertRow(-1);
-  data.map((row,index)=>{
-    let newCell = newRow.insertCell();
-    let newText = document.createTextNode(row);
-    newCell.appendChild(newText);
-  })
+function composeFinaliData(initialData){
+  let uniqueNames = getUniqueValues(initialData,0);
+  let uniqueDates = getUniqueValues(initialData,1);
+  let finalData = [];
+  for(let name of uniqueNames){
+    let row = [];
+    row.push(name);
+    for(let date of uniqueDates){
+      let index = uniqueDates.findIndex(i => i === date) + 1;
+      let hours = getHours(initialData, name, date);
+      row.splice(index, 0, hours);
+    }
+    finalData.push(row);
+  }
+  addHeader(finalData, uniqueDates);
+  return finalData;
+}
+
+function addHeader(finalData, uniqueDates) {
+  let header = [];
+  header = [...uniqueDates];
+  header.splice(0, 0, "Name / Date");
+  finalData.splice(0, 0, header);
+}
+
+function getHours(initialData, name, date) {
+  for(let row of initialData) {
+    if(row[0] == name && row[1] == date) {
+      return row[2];
+    }
+  }
+  return 0;
+}
+
+function getUniqueValues (initialData,sequenceNumb){
+  let numbers = [];
+  for(let item of initialData){
+    numbers.push(item[sequenceNumb]);
+    
+  }
+  return ([...new Set(numbers)]);
+}
+
+function removeHeader(initialData){
+  initialData.shift();
 }
