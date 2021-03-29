12ec56179522ec55217d9b7947935dbb7db47fa9
Vlad <vladikdavidov98>
2021-03-29 16:47:20 +0300

    completed

--- js/index.js
+++ js/index.js
@@ -1,19 +1,38 @@
 
-let btn = document.getElementById('btn-upload-csv').addEventListener('click',()=>{
-  Papa.parse(document.getElementById('upload-csv').files[0],{
-    download:true,
-    header:false,
-    complete: function(results){
-      main(results.data);
-
+let btn = document.getElementById('btn-upload-csv').addEventListener('click',()=> {
+    removeErrorMessage("no_file");
+    try {
+      Papa.parse(document.getElementById('upload-csv').files[0],{
+        download:true,
+        header:false,
+        complete: function(results){
+          main(results.data);
+        }
+      });
+    } catch (e) {
+      addErrorMessage("no_file");
     }
-  });
 });
 
-function main(initialData){
-  removeHeader(initialData);
-  let finaliData = composeFinaliData(initialData);
-  console.log(finaliData);
+function main(initialData) {
+  removeErrorMessage("error");
+  try {
+    removeHeader(initialData);
+    let finaliData = composeFinaliData(initialData);
+    composeCSV(finaliData);
+  } catch (e) {
+    addErrorMessage("error");
+  }
+}
+
+function composeCSV(finaliData) {
+  let csvContent = "data:text/csv;charset=utf-8," + finaliData.map(e => e.join(",")).join("\n");
+  var encodedUri = encodeURI(csvContent);
+  var link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "converted_data.csv");
+  document.body.appendChild(link);
+  link.click(); 
 }
 
 function composeFinaliData(initialData){
@@ -36,9 +55,19 @@ function composeFinaliData(initialData){
 
 function addHeader(finalData, uniqueDates) {
   let header = [];
-  header = [...uniqueDates];
+  let convertedDates = getConvertedDates(uniqueDates);
+  header = [...convertedDates];
   header.splice(0, 0, "Name / Date");
   finalData.splice(0, 0, header);
+}
+
+
+function getConvertedDates(uniqueDates){
+  let convertedDates = [];
+  for(let date of uniqueDates) {
+      convertedDates.push(new Date(date).toISOString().substring(0,10));
+  }
+  return convertedDates;
 }
 
 function getHours(initialData, name, date) {
@@ -51,14 +80,21 @@ function getHours(initialData, name, date) {
 }
 
 function getUniqueValues (initialData,sequenceNumb){
-  let numbers = [];
+  let items = [];
   for(let item of initialData){
-    numbers.push(item[sequenceNumb]);
-    
+    items.push(item[sequenceNumb]);
   }
-  return ([...new Set(numbers)]);
+  return ([...new Set(items)]);
 }
 
 function removeHeader(initialData){
   initialData.shift();
+}
+
+function removeErrorMessage(id) {
+  document.getElementById(id).classList.add('hide');
+}
+
+function addErrorMessage(id) {
+  document.getElementById(id).classList.remove('hide');
 }
